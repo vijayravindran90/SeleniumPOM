8541ce031ee19e2556a4aa6820e294b239d22690
vijayravindran90 <108650169+vijayravindran90@users.noreply.github.com>
2023-03-29 09:54:30 +0530

    Update BaseConfig.java

--- src/test/java/setup/BaseConfig.java
+++ src/test/java/setup/BaseConfig.java
@@ -24,7 +24,8 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseConfig {
     public static WebDriver driver;
-    public static final String BASE_URL = "https://c0.avaamo.com/web_channels/d0880907-3408-4c8d-9226-7648880a94fa/demo.html?banner=true&demo=true&banner_text=%20&banner_title=This%20is%20how%20the%20chat%20agent%20shows%20up";
+   // public static final String BASE_URL = "https://c0.avaamo.com/web_channels/d0880907-3408-4c8d-9226-7648880a94fa/demo.html?banner=true&demo=true&banner_text=%20&banner_title=This%20is%20how%20the%20chat%20agent%20shows%20up";
+     public static final String BASE_URL="https://chat.openai.com/auth/login";
     String chromeDriverPath = "chromedriver";
     String firefoxDriverPath = "geckodriver";
 
